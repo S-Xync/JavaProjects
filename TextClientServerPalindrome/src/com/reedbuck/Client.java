6c9b192d9afbae9b88f312eb1ac11f46ee8c513d
S-Xync <saikumarimmadi@users.noreply.github.com>
2016-10-29 20:59:55 +0530

    Created the Project

--- TextClientServerPalindrome/src/com/reedbuck/Client.java
+++ TextClientServerPalindrome/src/com/reedbuck/Client.java
@@ -1,10 +1,6 @@
 package com.reedbuck;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -12,41 +8,36 @@ import java.net.Socket;
  */
 public class Client {
     public static void main(String[] args) {
-        int port=6500;
+        System.out.println("Client trying to connect to Palindrome Server at port : "+6500);
         try {
-            ServerSocket serverSocket=new ServerSocket(port);
-            System.out.println("Palindrome Checking Server started at port : "+port);
-            boolean flag=true;
-            while (flag){
-                Socket clientSocket=serverSocket.accept();
-                BufferedReader serverIn=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter serverOut=new PrintWriter(clientSocket.getOutputStream(),true);
-                String line=null;
-                while((line=serverIn.readLine())!=null){
-                    if (line.equalsIgnoreCase("bye")){
-                        flag=false;
-                        System.out.println("Palindrome Server is shutting down");
-                        serverOut.println("Palindrome Server has shut down");
-                        serverOut.flush();
-                        break;
-                    }else {
-                        System.out.println("Server got word : "+line);
-                        serverOut.println(palindromeChecker(line));
-                        serverOut.flush();
-                    }
-                }
+            Socket clientSocket=new Socket("localhost",6500);
+            BufferedReader clientIn=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter clientOut=new PrintWriter(clientSocket.getOutputStream());
+            FileReader fr=new FileReader("./src/word.txt");
+            FileWriter fw=new FileWriter("./src/answer.txt");
+            BufferedReader br=new BufferedReader(fr);
+            String line="";
+            while ((line=br.readLine())!=null){
+                System.out.println("Sending : "+line);
+                clientOut.println(line);
+                clientOut.flush();
             }
+            clientOut.println("Bye");
+            clientOut.flush();
+            while ((line=clientIn.readLine())!=null){
+                fw.write(line+"\n");
+            }
+            System.out.println("Results written to answer.txt");
+            clientIn.close();
+            clientOut.close();
+            clientSocket.close();
+            fr.close();
+            br.close();
+            fw.close();
         } catch (IOException e) {
             System.out.println(e);
         }
+
     }
-    public static int palindromeChecker(String string){
-        int n=string.length();
-        for (int i=0;i<n/2;i++){
-            if (string.charAt(i)!=string.charAt(n-i)){
-                return 0;
-            }
-        }
-        return 1;
-    }
+
 }
--- TextClientServerPalindrome/src/com/reedbuck/Server.java
+++ TextClientServerPalindrome/src/com/reedbuck/Server.java
@@ -1,10 +1,57 @@
 package com.reedbuck;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by saikumar on 28/10/16 at 2:14 PM.
  */
 public class Server {
     public static void main(String[] args) {
+        int port=6500;
+        try {
+            ServerSocket serverSocket=new ServerSocket(port);
+            System.out.println("Palindrome Checking Server started at port : "+port);
+            boolean flag=true;
+            while (flag){
+                Socket clientSocket=serverSocket.accept();
+                BufferedReader serverIn=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter serverOut=new PrintWriter(clientSocket.getOutputStream(),true);
+                String line="";
+                while((line=serverIn.readLine())!=null){
+                    if (line.equalsIgnoreCase("bye")){
+                        flag=false;
+                        System.out.println("Palindrome Server is shutting down");
+                        serverOut.println("Palindrome Server has shut down");
+                        serverOut.flush();
+                        serverIn.close();
+                        serverOut.close();
+                        break;
+                    }else {
+                        System.out.println("Server got word : "+line);
+                        serverOut.println(palindromeChecker(line));
+                        serverOut.flush();
+                    }
+                }
+            }
+            serverSocket.close();
 
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+    }
+    public static int palindromeChecker(String string){
+        int n=string.length();
+        for (int i=0;i<n/2;i++){
+            if (string.charAt(i)!=string.charAt(n-i-1)){
+                return 0;
+            }
+        }
+        return 1;
     }
 }
